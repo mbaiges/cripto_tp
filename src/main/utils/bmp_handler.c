498f43371043a30b8da7bf2e651dcc9c7225d1e4
euge <epineiro@itba.edu.ar>
2021-05-14 12:52:51 -0300

    printed values

--- src/main/utils/bmp_handler.c
+++ src/main/utils/bmp_handler.c
@@ -27,19 +27,19 @@ int load_image(const char * filename, image_composition * img_comp) {
     printf("\tfile_size[1]=%x\n", img_comp->header.file_size[1]);
     printf("\tfile_size[2]=%x\n", img_comp->header.file_size[2]);
     printf("\tfile_size[3]=%x\n", img_comp->header.file_size[3]);
-    // printf("\treserved=%u", image_comp->header.reserved);
+    // printf("\treserved=%x", image_comp->header.reserved);
     printf("\tdata_offset=%x\n", img_comp->header.data_offset);
-    printf("\theader_size=%u\n", img_comp->header.header_size);
-    printf("\timage_width=%u\n", img_comp->header.image_width);
-    printf("\timage_height=%u\n", img_comp->header.image_height);
-    printf("\timage_color_planes=%u\n", img_comp->header.image_color_planes);
-    printf("\timage_bpp=%u\n", img_comp->header.image_bpp);
-    printf("\timage_compression=%u\n", img_comp->header.image_compression);
-    printf("\timage_size=%u\n", img_comp->header.image_size);
-    printf("\timage_pixels_per_meter_X=%u\n", img_comp->header.image_pixels_per_meter_X);
-    printf("\timage_pixels_per_meter_Y=%u\n", img_comp->header.image_pixels_per_meter_Y);
-    printf("\timage_colors_used=%u\n", img_comp->header.image_colors_used);
-    printf("\timage_important_colors=%u\n", img_comp->header.image_important_colors);
+    printf("\theader_size=%x\n", img_comp->header.header_size);
+    printf("\timage_width=%x\n", img_comp->header.image_width);
+    printf("\timage_height=%x\n", img_comp->header.image_height);
+    printf("\timage_color_planes=%x\n", img_comp->header.image_color_planes);
+    printf("\timage_bpp=%x\n", img_comp->header.image_bpp);
+    printf("\timage_compression=%x\n", img_comp->header.image_compression);
+    printf("\timage_size=%x\n", img_comp->header.image_size);
+    printf("\timage_pixels_per_meter_X=%x\n", img_comp->header.image_pixels_per_meter_X);
+    printf("\timage_pixels_per_meter_Y=%x\n", img_comp->header.image_pixels_per_meter_Y);
+    printf("\timage_colors_used=%x\n", img_comp->header.image_colors_used);
+    printf("\timage_important_colors=%x\n", img_comp->header.image_important_colors);
 
     printf("\n");
 
